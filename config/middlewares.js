a446bdd8213d92c5f62c96973bbf97c8a29ec84e
stephenbliz <uzomaebuka11@gmail.com>
2024-05-28 12:24:20 +0100

    updated middleware security

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,30 +1,26 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  module.exports = [
-    // ...
-    {
-      name: 'strapi::security',
-      config: {
-        contentSecurityPolicy: {
-          useDefaults: true,
-          directives: {
-            'connect-src': ["'self'", 'https:'],
-            'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
-            'media-src': [
-              "'self'",
-              'data:',
-              'blob:',
-              'market-assets.strapi.io',
-              'res.cloudinary.com',
-            ],
-            upgradeInsecureRequests: null,
-          },
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
         },
       },
     },
-    // ...
-  ],
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
